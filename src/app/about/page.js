0110925ae3849528d7edb27c4f7741dc3f45c966
Joao <joaopedromello13@hotmail.com>
2025-02-28 17:16:54 -0300

    Historia pokemon

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -3,41 +3,37 @@ export default async function About() {
         <>
             <div className="container mt-5 justify">
                 <h1 className="text-center mb-4">History</h1>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                    Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s,
-                    when an unknown printer took a galley of type and scrambled it to make a type
-                    specimen book. It has survived not only five centuries, but also the leap into
-                    electronic typesetting, remaining essentially unchanged. It was popularised in the
-                    1960s with the release of Letraset sheets containing Lorem Ipsum passages,
-                    and more recently with desktop publishing software like Aldus PageMaker including
-                    versions of Lorem Ipsum.
+                <p>A Origem de Pokémon
+                    A franquia Pokémon foi criada por Satoshi Tajiri e
+                    lançada pela Game Freak e Nintendo em 1996 com os jogos Pokémon Red & Green no Japão (Red & Blue no Ocidente).
+                    Tajiri era fascinado por colecionar insetos na infância, o que inspirou o conceito de capturar e catalogar
+                    criaturas em um mundo virtual.
+                    Pokémon rapidamente se tornou um fenômeno mundial,
+                    expandindo-se para jogos, anime, mangás, filmes, brinquedos e um império multimídia.
                 </p>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                    Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s,
-                    when an unknown printer took a galley of type and scrambled it to make a type
-                    specimen book. It has survived not only five centuries, but also the leap into
-                    electronic typesetting, remaining essentially unchanged. It was popularised in the
-                    1960s with the release of Letraset sheets containing Lorem Ipsum passages,
-                    and more recently with desktop publishing software like Aldus PageMaker including
-                    versions of Lorem Ipsum.
+                <p>O Que é a Pokédex?
+                    A Pokédex é um dispositivo dentro do universo Pokémon que funciona como uma enciclopédia eletrônica,
+                    registrando informações sobre os Pokémon capturados ou encontrados pelo treinador. Nos jogos,
+                    a Pokédex sempre foi um dos principais objetivos: completar sua coleção registrando todos os Pokémon
+                    disponíveis.
+
+                    <br>A Primeira Pokédex (Geração 1 – 1996)</br>
+                    Nos primeiros jogos (Pokémon Red & Blue), a Pokédex era apresentada pelo Professor Oak,
+                    e seu objetivo era registrar os 151 Pokémon da região de Kanto. O design era simples, e a
+                    funcionalidade principal era listar as criaturas encontradas.
                 </p>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                    Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s,
-                    when an unknown printer took a galley of type and scrambled it to make a type
-                    specimen book. It has survived not only five centuries, but also the leap into
-                    electronic typesetting, remaining essentially unchanged. It was popularised in the
-                    1960s with the release of Letraset sheets containing Lorem Ipsum passages,
-                    and more recently with desktop publishing software like Aldus PageMaker including
-                    versions of Lorem Ipsum.
-                </p>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                    Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s,
-                    when an unknown printer took a galley of type and scrambled it to make a type
-                    specimen book. It has survived not only five centuries, but also the leap into
-                    electronic typesetting, remaining essentially unchanged. It was popularised in the
-                    1960s with the release of Letraset sheets containing Lorem Ipsum passages,
-                    and more recently with desktop publishing software like Aldus PageMaker including
-                    versions of Lorem Ipsum.
+                <p>Curiosidades sobre a Pokédex
+                    📌 Alguns registros da Pokédex nos jogos são assustadores, como Hypno, que sequestra crianças,
+                    ou Yamask, que carrega a máscara de um humano falecido.
+
+                    📌 A maior Pokédex já registrada em um jogo foi a de Pokémon HOME, 
+                    que permite armazenar mais de 1000 Pokémon diferentes.
+
+                    📌 Alguns Pokémon têm descrições inconsistentes ou exageradas, como Magcargo, 
+                    que supostamente é mais quente que o Sol.
+
+                    📌 A Pokédex não é 100% precisa, já que foi feita por professores e treinadores Pokémon,
+                     que podem errar ou exagerar em suas descrições.
                 </p>
             </div>
         </>
