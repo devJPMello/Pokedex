6a97b816fc6b0346d5907962eb1c8f0552706147
Joao <joaopedromello13@hotmail.com>
2025-02-28 17:21:54 -0300

    Alteraçao na about

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -3,7 +3,7 @@ export default async function About() {
         <>
             <div className="container mt-5 justify">
                 <h1 className="text-center mb-4">History</h1>
-                <p>A Origem de Pokémon
+                <p>A Origem de Pokémon<br />
                     A franquia Pokémon foi criada por Satoshi Tajiri e
                     lançada pela Game Freak e Nintendo em 1996 com os jogos Pokémon Red & Green no Japão (Red & Blue no Ocidente).
                     Tajiri era fascinado por colecionar insetos na infância, o que inspirou o conceito de capturar e catalogar
@@ -11,26 +11,26 @@ export default async function About() {
                     Pokémon rapidamente se tornou um fenômeno mundial,
                     expandindo-se para jogos, anime, mangás, filmes, brinquedos e um império multimídia.
                 </p>
-                <p>O Que é a Pokédex?
+                <p>O Que é a Pokédex?<br />
                     A Pokédex é um dispositivo dentro do universo Pokémon que funciona como uma enciclopédia eletrônica,
                     registrando informações sobre os Pokémon capturados ou encontrados pelo treinador. Nos jogos,
                     a Pokédex sempre foi um dos principais objetivos: completar sua coleção registrando todos os Pokémon
                     disponíveis.
 
-                    <br>A Primeira Pokédex (Geração 1 – 1996)</br>
+                    A Primeira Pokédex (Geração 1 – 1996)<br />
                     Nos primeiros jogos (Pokémon Red & Blue), a Pokédex era apresentada pelo Professor Oak,
                     e seu objetivo era registrar os 151 Pokémon da região de Kanto. O design era simples, e a
                     funcionalidade principal era listar as criaturas encontradas.
                 </p>
-                <p>Curiosidades sobre a Pokédex
+                <p>Curiosidades sobre a Pokédex<br />
                     📌 Alguns registros da Pokédex nos jogos são assustadores, como Hypno, que sequestra crianças,
-                    ou Yamask, que carrega a máscara de um humano falecido.
+                    ou Yamask, que carrega a máscara de um humano falecido.<br />
 
                     📌 A maior Pokédex já registrada em um jogo foi a de Pokémon HOME, 
-                    que permite armazenar mais de 1000 Pokémon diferentes.
+                    que permite armazenar mais de 1000 Pokémon diferentes.<br />
 
                     📌 Alguns Pokémon têm descrições inconsistentes ou exageradas, como Magcargo, 
-                    que supostamente é mais quente que o Sol.
+                    que supostamente é mais quente que o Sol.<br />
 
                     📌 A Pokédex não é 100% precisa, já que foi feita por professores e treinadores Pokémon,
                      que podem errar ou exagerar em suas descrições.
